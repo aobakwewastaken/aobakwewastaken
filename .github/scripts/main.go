e3385cb3bff7b49357041d11c8518c7068b115f9
OkayAobakwe <okayaobakwe@gmail.com>
2023-04-20 17:25:57 +0200

    chore(readme): adds my content to the readme

--- .github/scripts/main.go
+++ .github/scripts/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
 type Story struct {
@@ -12,6 +17,21 @@ type Story struct {
 	URL string `json:"url"`
 }
 func main() {
+	var readMeContent string
+	accessToken := os.Getenv("GITHUB_ACCESS_TOKEN")
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{ AccessToken: accessToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	owner := "aobakwewastaken"
+	repo := "aobakwewastaken"
+	readme, _, err := client.Repositories.GetReadme(ctx, owner, repo, &github.RepositoryContentGetOptions{})
+	if err != nil {
+		fmt.Println("Failed to fetch readme: ", err)
+	}
+
 	resp, err := http.Get("https://hacker-news.firebaseio.com/v0/topstories.json")
 	if err != nil {
 		fmt.Println("Failed to fetch top stories: ", err)
@@ -26,7 +46,6 @@ func main() {
 		fmt.Println("Failed to decode response: ", err)
 		return
 	}
-
 	for i := 0; i < 10 && i < len(storyIDs); i++ {
 		storyID := storyIDs[i]
 		storyResp, err := http.Get(fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%d.json", storyID))
@@ -42,6 +61,22 @@ func main() {
 			fmt.Println("Failed to decode story:", err)
 			continue
 		}
-		fmt.Printf("Title: %s\nURL: %s\n\n", story.Title, story.URL)
+		readMeContent += fmt.Sprintf("Title: %s\nURL: %s\n\n", story.Title, story.URL)
 	}
+	currentReadMeContent, err := readme.GetContent()
+
+	if err != nil {
+		fmt.Println("Failed to get content: ", err)
+	}
+	currentReadMeContent = readMeContent
+	opts := &github.RepositoryContentFileOptions{
+		Message: github.String("Update README"),
+		Content: []byte(currentReadMeContent),
+		SHA:     readme.SHA,
+	}
+	_, _, err = client.Repositories.UpdateFile(ctx, owner, repo, *readme.Path, opts)
+	if err != nil {
+		fmt.Println("Failed to update Readme: ", err)
+	}
+	
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## Aobakwe
+
+# Top Stories on hackernews
